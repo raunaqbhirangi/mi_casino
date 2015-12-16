f608a74169ac0492f719590cf531bdf3416d9e31
raunaqbhirangi <raunaqbhirangi@gmail.com>
2015-12-16 08:46:57 +0530

    portal done. warnings and redirection left

--- index.js
+++ index.js
@@ -3,8 +3,167 @@ var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
-var session = require('client-sessions');
-
+var game_data = {
+	'poker':{
+		'slots' : 8,
+		'tables' :{
+			'number' : 6,
+			'people' : 1,
+		},
+	},
+	'blackjack':{
+		'slots' : 8,
+		'tables' :{
+			'number' : 6,
+			'people' : 1,
+		},
+	},
+	'derby':{
+		'slots' : 8,
+		'tables' :{
+			'number' : 4,
+			'people' : 2,
+		},
+	},
+	'roulette':{
+		'slots' : 8,
+		'tables' :{
+			'number' : 1,
+			'people' : 3,
+		},
+	},
+};
+var filled_slots = [
+{
+	'day':1,
+	'game':[{
+		'name': 'poker',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'blackjack',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'derby',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'roulette',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},]
+},
+{
+	'day':2,
+	'game':[{
+		'name': 'poker',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		},
+		{
+			'slotno':5,
+			'tables':[2,3]
+		}]
+	},
+	{
+		'name': 'blackjack',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'derby',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'roulette',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},]
+},
+{
+	'day':3,
+	'game':[{
+		'name': 'poker',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'blackjack',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'derby',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'roulette',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},]
+},
+{
+	'day':4,
+	'game':[{
+		'name': 'poker',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'blackjack',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'derby',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},
+	{
+		'name': 'roulette',
+		'slots':[{
+			'slotno':0,
+			'tables':[],
+		}]
+	},]
+},
+];
+var CURR_DAY = 1;
 http.listen(8000,function(){
 	console.log('Server listening on Port 8000');
 });
@@ -14,13 +173,6 @@ var connection = mysql.createConnection({
 	password:'iamthebest111',
 	database:'mi_casino',
 });
-
-app.use(session({
-	cookieName : 'session',
-	secret : 'afwqewqsfwefda',
-	duration : 5*60*1000,
-	activeDuration : 2*60*1000,
-}));
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -33,7 +185,149 @@ app.get('/java.js',function(req,res){
 	res.sendFile(__dirname+'/java.js');
 });
 
+app.get('/next_page', function(req,res){
+	res.sendFile(__dirname+'/secondpage.html');
+});
+
+app.get('/jquery',function(req,res){
+	res.sendFile(__dirname+'/jquery-1.11.3.min.js');
+});
+
 app.post('/new_reg', function(req,res){
-	console.log(req.data);
-})
+	console.log(req.body);
+	var mino = req.body.mino;
+	var game = {};
+	var data = {};
+	game.game = req.body.game;
+	game.data = game_data[game.game];
+	var query = 'SELECT * from '+game.game+' WHERE mi_no = "'+mino+'"';
+	connection.query(query,function(err,rows,fields){
+		if(!!err)
+		{
+			console.log(err);
+			data.error = err;
+			data.status = false;
+		}
+		else if(rows.length!=0)
+		{
+			console.log(rows);
+			data.status = false;
+			data.data = rows;
+			data.game = game;
+		}
+		else if(rows.length == 0)
+		{
+			data.status = true;
+			data.data = {};
+			data.data.mi_no = mino;
+			data.game = game;
+		}
+		res.json(data);
+	});
+});
+
+app.post('/check_fill',function(req,res){
+	var slotno = req.body.slotno;
+	console.log('around here');
+	console.log(slotno);
+	var game = req.body.game;
+	for (var i = 0; i < filled_slots[CURR_DAY-1].game.length; i++) {
+		if(filled_slots[CURR_DAY-1].game[i].name == game)
+		{
+			var curr_game = filled_slots[CURR_DAY-1].game[i];
+			break;
+		}
+	};
+	var data = {
+		'tables':[],
+	};
+	data.total = game_data[game].tables.number;
+	data.slot_no = slotno;
+	for (var i = 0; i < curr_game.slots.length; i++) {
+		if(curr_game.slots[i].slotno == slotno)
+		{
+			var curr_slot = curr_game.slots[i];
+			for (var j = 0; j < curr_slot.tables.length; j++) {
+				data.tables.push(curr_slot.tables[j]);
+			}
+		}
+	}
+	res.json(data);
+});
+
+app.post('/add_entry',function(req,res){
+	var data = req.body;
+	console.log(data);
+	var send_data = {};
+	var query = 'INSERT INTO '+data.game+' VALUES ("'+data.mino+'",'+data.slotno+','+data.tableno;
+	if (data.game == 'poker') 
+	{	
+		query+=',"'+data.phoneno+'",'+data.exp;
+	}
+	query+=','+CURR_DAY+')';
+	console.log(query);
+	connection.query(query,function(err,rows,fields){
+		if(!!err)
+		{
+			send_data.error = err;
+			send_data.msg = 'Error adding user. Please try again.';
+		}
+		else
+		{
+			send_data.error = 0;
+			send_data.msg = 'User Added Successfully';
+		}
+	});
+	query = 'SELECT * from '+data.game+' WHERE table_no='+data.tableno+' AND slot_no='+data.slotno;
+	console.log(query);	
+	connection.query(query,function(err,rows,fields){
+		if(!!err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			console.log('row='+rows.length);
+			console.log(game_data[data.game].tables.people);
+			if(rows.length == game_data[data.game].tables.people)
+			{
+				console.log('hi i got here');
+				for (var i = 0; i < filled_slots[CURR_DAY-1].game.length; i++) {
+					if(filled_slots[CURR_DAY-1].game[i].name == data.game)
+					{
+						var curr_game = filled_slots[CURR_DAY-1].game[i];
+						break;
+					}
+				};
+				var slot_flag = 0;
+				var slot_pos = 0;
+				for (var i = 0; i < curr_game.slots.length; i++) {
+					if(curr_game.slots[i].slotno == data.slotno)
+					{
+						console.log('now here');
+						slot_flag = 1;
+						slot_pos = i;
+						break;
+					}
+				};
+				if (slot_flag!=0) 
+				{
+					curr_game.slots[slot_pos].tables.push(data.tableno);
+				}
+				else
+				{
+					var new_data = {
+						'slotno':0,
+						'tables':[],
+					}
+					new_data.slotno = data.slotno;
+					new_data.tables.push(data.tableno);
+					console.log(new_data);
+					curr_game.slots.push(new_data);
+				}
+			}
+		}
+		res.json(send_data);
+	});
+});
 
--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-
+	$('#game_data').hide();
 })
 
-var submit_function = function(){
+var check_function = function(){
 	var mino = $('#mino_input').val().toUpperCase();
 	var game = $('#game_select').val();
 	var mino_check = /^MI-([A-Z]){3}-([0-9]){3}$/m;
@@ -14,20 +14,133 @@ var submit_function = function(){
 		$.ajax({
 		  dataType: 'json',
 	      method : 'POST',
-	      url : 'new_feed',
+	      url : 'new_reg',
 	      data : send_data,
 	      responseType : "json",
 	      headers: {  
 	      "Content-Type": 'application/x-www-form-urlencoded'
 	  	  },
 	  	  success : function(data){
-	  	  	console.log(data)
-	  	  };
-  	}
+	  	  	console.log(data);
+	  	  	if(!!data.error)
+	  	  	{
+	  	  		console.log(data.error);
+	  	  	}
+	  	  	else if(data.status == false)
+	  	  	{
+	  	  		console.log(data.data[0]);		//existing entry
+	  	  	}
+	  	  	else
+	  	  	{
+	  	  		$('#personal_data').hide();
+	  	  		$('#game_data').show();
+	  	  		if(data.game.game != 'poker')
+	  	  		{
+	  	  			$('#poker_exp').hide();
+	  	  			$('#phoneno').hide();
+	  	  		}
+	  	  		var send_data = '';
+	  	  		for (var i = 0; i < data.game.data.slots; i++) {
+	  	  			var slot_no = i+1;
+	  	  			send_data = 'game='+data.game.game+'&slotno='+slot_no;
+	  	  			$.ajax({
+					  dataType: 'json',
+				      method : 'POST',
+				      url : 'check_fill',
+				      data : send_data,
+				      responseType : "json",
+				      headers: {  
+				      "Content-Type": 'application/x-www-form-urlencoded'
+				  	  },
+				  	  success : function(data){
+				  	  	if(data.total != data.tables.length)
+				  	  	{
+				  	  		$('#slotno').append($('<option>',{
+			  	  				value: data.slot_no,
+			  	  				text: data.slot_no,
+			  	  			}));
+				  	  	}
+				  	  }
+				  	});
+	  	  		}
+	  	  		table_fill();
+	  	  		$('#data_display').html('MI NO. - '+mino+'<br>'+'Game - '+game);
+	  	  	}
+	  	  }
   	});
 	}
 	else
 	{
 		console.log('Invalid mino');
 	}
+}
+
+var table_fill = function(){
+	var send_data = '';
+	var slotno = $('#slotno').val();
+	var game = $('#game_select').val();
+	if(slotno == null)
+		send_data = 'game='+game+'&slotno=1';
+	else
+		send_data = 'game='+game+'&slotno='+slotno;
+	$.ajax({
+		  dataType: 'json',
+	      method : 'POST',
+	      url : 'check_fill',
+	      data : send_data,
+	      responseType : "json",
+	      headers: {  
+	      "Content-Type": 'application/x-www-form-urlencoded'
+	  	  },
+	  	  success : function(data){
+	  	  	//console.log(data);
+	  	  	$('#tableno').find('option').remove();
+	  	  	for (var i = 0; i < data.total; i++) {
+	  	  		var flag = 0;
+	  	  		for (var j = 0; j < data.tables.length; j++) {
+	  	  			if(data.tables[j] == i+1)
+	  	  			{
+	  	  				flag = 1;
+	  	  			}
+	  	  		};
+	  	  		if(!flag){
+	  	  			$('#tableno').append($('<option>',{
+	  	  				value: i+1,
+	  	  				text: i+1,
+	  	  			}));
+	  	  		}
+	  	  	}
+	  	}
+	});
+}
+
+var submit_function = function(){
+	var mino = $('#mino_input').val().toUpperCase();
+	var game = $('#game_select').val();
+	var slotno = $('#slotno').val();
+	if(slotno==null)
+		slotno = 1;
+	var tableno = $('#tableno').val();
+	var send_data = '';
+	send_data = 'mino='+mino+'&game='+game+'&slotno='+slotno+'&tableno='+tableno;
+	if(game == 'poker')
+	{
+		var exp = $('input[name="exp"]:checked').val();
+		var phoneno = $('#phoneno_input').val();
+		send_data+='&exp='+exp+'&phoneno='+phoneno;
+	}
+	console.log(send_data);
+	$.ajax({
+	  dataType: 'json',
+      method : 'POST',
+      url : 'add_entry',
+      data : send_data,
+      responseType : "json",
+      headers: {  
+      "Content-Type": 'application/x-www-form-urlencoded'
+  	  },
+  	  success : function(data){
+  	  	console.log(data);
+  	  }
+  	});
 }
